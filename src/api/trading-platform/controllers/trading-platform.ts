7941c2f2150e5df8af5df7c80bb900e720b0add5
elefthee <e.eleftheriadis@ingotbrokers.com>
2025-02-12 15:13:11 +0200

    don't break please

---
+++ src/api/trading-platform/controllers/trading-platform.ts
@@ -0,0 +1,7 @@
+/**
+ * trading-platform controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::trading-platform.trading-platform');
---
+++ src/api/trading-platform/routes/trading-platform.ts
@@ -0,0 +1,7 @@
+/**
+ * trading-platform router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::trading-platform.trading-platform');
